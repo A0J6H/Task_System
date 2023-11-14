aae1f9f9a4e20f3040f59752b6ed62a74912390e
A0J6H <andrewhilton2006@gmail.com>
2023-11-14 21:25:55 +0000

    Add project files.

---
+++ Task_System/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Task_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(93)))), ((int)(((byte)(62)))));
+            this.button1.BackgroundImage = global::Task_System.Properties.Resources.AddButton;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(43, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 104);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Task_System.Properties.Resources.AddTask;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 190);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(190)))), ((int)(((byte)(134)))));
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 87);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(388, 510);
+            this.flowLayoutPanel1.TabIndex = 5;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 202);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(190)))), ((int)(((byte)(134)))));
+            this.BackgroundImage = global::Task_System.Properties.Resources.Frame_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(549, 729);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Organiser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Task_System/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_System
+{
+    public partial class Form1 : Form
+    {
+        List<Button> tasks = new List<Button>();
+        bool x = true;
+        public Form1()
+        {
+            //size 420, 693
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneVScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = "Revision";
+            if (x == true)
+            {
+                name = "Homework";
+                x = false;
+            }
+            
+            Panel dynamicPanel = new Panel();
+            dynamicPanel.Location = new System.Drawing.Point(26, 12);
+            dynamicPanel.Name = "Panel1";
+            dynamicPanel.Size = new System.Drawing.Size(175, 202);
+            //TextBox textBox1 = new TextBox();
+            //textBox1.Location = new Point(10, 10);
+            //textBox1.Text = "I am a TextBox5";
+            //textBox1.Size = new Size(200, 30);
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.BackgroundImage = Image.FromFile("C:\\Users\\Andrew\\source\\repos\\Task_System\\Task_System\\Resources\\GreenTask.png");
+            pictureBox.Location = new Point(20, 9);
+            pictureBox.Size = new Size(154, 190);
+            pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+            Button button = new Button();
+            button.Name = name;
+            button.Click += Button_Click;
+            button.Text = name;
+            button.Location = new Point(20,50);
+            button.Size = new Size(80,50);
+            button.FlatStyle = FlatStyle.Flat;
+            //CheckBox checkBox1 = new CheckBox();
+            //checkBox1.Location = new Point(10, 50);
+            //checkBox1.Text = "Check Me";
+            //checkBox1.Size = new Size(200, 30);
+            //dynamicPanel.Controls.Add(textBox1);
+            dynamicPanel.Controls.Add(button);
+            dynamicPanel.Controls.Add(pictureBox);
+            
+            //dynamicPanel.Controls.Add(checkBox1);
+            Controls.Add(dynamicPanel);
+            flowLayoutPanel1.Controls.Add(dynamicPanel);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Name == "Homework")
+            {
+                MessageBox.Show("ITS HOMEWORK");
+            }
+            else if (btn.Name == "Revision")
+            {
+                MessageBox.Show("ITS REVISION");
+            }
+            
+        }
+    }
+}
