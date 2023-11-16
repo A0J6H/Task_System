8e3f3c0fbd9c3ddbaa22908d9fb209e55f6b2667
A0J6H <andrewhilton2006@gmail.com>
2023-11-16 22:48:19 +0000

    yo yo yo

--- Task_System/Form1.Designer.cs
+++ Task_System/Form1.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -65,17 +68,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(190)))), ((int)(((byte)(134)))));
-            this.flowLayoutPanel1.Controls.Add(this.panel1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 87);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(388, 552);
-            this.flowLayoutPanel1.TabIndex = 5;
-            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
-            // 
             // panel1
             // 
             this.panel1.Controls.Add(this.button1);
@@ -85,16 +77,54 @@
             this.panel1.Size = new System.Drawing.Size(175, 202);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // panel3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
-                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(77, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.panel3.AutoScroll = true;
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Location = new System.Drawing.Point(12, 87);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(388, 552);
+            this.panel3.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(210, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(441, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 97);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(619, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 71);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -103,17 +133,20 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(190)))), ((int)(((byte)(134)))));
             this.BackgroundImage = global::Task_System.Properties.Resources.Frame_1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(549, 729);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(692, 729);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel3);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Organiser";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,9 +155,12 @@
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Task_System/Form1.cs
+++ Task_System/Form1.cs
@@ -17,6 +17,11 @@ namespace Task_System
         //List<Button> tasks = new List<Button>();
         List<int> cols = new List<int>();
         bool x = true;
+        int xposr = 184;
+        int xposl = 0;
+        int yposr = 7;
+        int yposl = 211;
+        int c = 1;
         public Form1()
         {
             //size 420, 693
@@ -55,21 +60,38 @@ namespace Task_System
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int tc;
+            
+            int lines = 17;
+            int tc = 0 ;
             Random rnd = new Random();
             int rc = rnd.Next(0, 3);
             string name = "Revision";
-            string path;
+            string path = string.Empty;
             if (x == true)
             {
                 name = "Homework";
                 x = false;
             }
-            
+            string f = "test";
+
             Panel dynamicPanel = new Panel();
-            dynamicPanel.Location = new System.Drawing.Point(26, 12);
+            if (c == 1)
+            {
+                dynamicPanel.Location = new System.Drawing.Point(xposr, yposr);
+                yposr += 110 + lines +90;
+                c -= 1;
+            }
+            else
+            {
+                dynamicPanel.Location = new System.Drawing.Point(xposl, yposl);
+                yposl += 202 + lines-50;
+                c += 1;
+            }
+            
+            
+            
             dynamicPanel.Name = "Panel1";
-            dynamicPanel.Size = new System.Drawing.Size(175, 202);
+            dynamicPanel.Size = new System.Drawing.Size(175, 110+lines+40);
             //TextBox textBox1 = new TextBox();
             //textBox1.Location = new Point(10, 10);
             //textBox1.Text = "I am a TextBox5";
@@ -95,7 +117,7 @@ namespace Task_System
             }
             
             pictureBox.Location = new Point(20, 9);
-            pictureBox.Size = new Size(154, 190);
+            pictureBox.Size = new Size(154, 110+lines +40);
             pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
             //Creating the EDIT button//
             Button button = new Button();
@@ -131,10 +153,11 @@ namespace Task_System
             /////Creating the TITLE label//
             Label title = new Label();
             title.Text = "Title :";
+            title.MaximumSize = new Size(100,40);
             title.Font = new Font(large_font, style_);
             title.ForeColor = Color.FromArgb(cols[tc * 3], cols[tc * 3 + 1], cols[tc * 3 + 2]); 
             title.BackColor = Color.FromArgb(cols[rc * 3], cols[rc * 3 + 1], cols[rc * 3 + 2]);
-            title.Location = new Point(40,30);
+            title.Location = new Point(23,30);
             ////////////////////////////
             //Creating the DATE DUE label//
             Label date_due = new Label();
@@ -142,27 +165,63 @@ namespace Task_System
             date_due.Font = new Font(font, style_);
             date_due.ForeColor = Color.FromArgb(cols[tc * 3], cols[tc * 3 + 1], cols[tc * 3 + 2]);
             date_due.BackColor = Color.FromArgb(cols[rc * 3], cols[rc * 3 + 1], cols[rc * 3 + 2]);
-            date_due.Location = new Point(35, 65);
+            date_due.Location = new Point(20, 60);
             //Creating the DISPLAY DATE label//
             Label date = new Label();
             date.Text = "20/11/2023";
             date.Font = font;
+            date.Width = 90;
+            date.MaximumSize = new Size(90, 20);
             date.ForeColor = Color.FromArgb(cols[tc * 3], cols[tc * 3 + 1], cols[tc * 3 + 2]);
             date.BackColor = Color.FromArgb(cols[rc * 3], cols[rc * 3 + 1], cols[rc * 3 + 2]);
-            date.Location = new Point(65, 65);
+            date.Location = new Point(80, 60);
+            ///////////////////////////////////
+            //Creating DETAILS SUB label//
+            Label details_sub = new Label();
+            details_sub.Text = "Notes :";
+            details_sub.Font = new Font(font,style_);
+            details_sub.Width = 90;
+            details_sub.MaximumSize = new Size(90, 20);
+            details_sub.ForeColor = Color.FromArgb(cols[tc * 3], cols[tc * 3 + 1], cols[tc * 3 + 2]);
+            details_sub.BackColor = Color.FromArgb(cols[rc * 3], cols[rc * 3 + 1], cols[rc * 3 + 2]);
+            details_sub.Location = new Point(20, 90);
+            //Creating DETAILS textbox//
+            TextBox details = new TextBox();
+            details.BorderStyle = BorderStyle.None;
+            details.Cursor = DefaultCursor;
+            details.Multiline = true;
+            details.Font = font;
+            details.Text = f;
+            details.Size = new Size(150,200);
+            details.ForeColor = Color.FromArgb(cols[tc * 3], cols[tc * 3 + 1], cols[tc * 3 + 2]);
+            details.BackColor = Color.FromArgb(cols[rc * 3], cols[rc * 3 + 1], cols[rc * 3 + 2]);
+            details.Location = new Point(22, 110);
+            MessageBox.Show(details.Text.ToString());
+            MessageBox.Show(textBox1.Lines.Count().ToString());
+            /////////////////////////////
+            //Creating DATE CREATED label//
+            Label cdate = new Label();
+            cdate.Text = "16/11/2023";
+            cdate.Font = font;
+            cdate.ForeColor = Color.FromArgb(cols[tc * 3], cols[tc * 3 + 1], cols[tc * 3 + 2]);
+            cdate.BackColor = Color.FromArgb(cols[rc * 3], cols[rc * 3 + 1], cols[rc * 3 + 2]);
+            cdate.Location = new Point(65, 110 + lines + 10);
             //CheckBox checkBox1 = new CheckBox();
             //checkBox1.Location = new Point(10, 50);
             //checkBox1.Text = "Check Me";
             //checkBox1.Size = new Size(200, 30);
             //dynamicPanel.Controls.Add(textBox1);
+            dynamicPanel.Controls.Add(cdate);
+            dynamicPanel.Controls.Add(details);
             dynamicPanel.Controls.Add(button);
             dynamicPanel.Controls.Add(title);
-            dynamicPanel.Controls.Add(date_due);
             dynamicPanel.Controls.Add(date);
+            dynamicPanel.Controls.Add(date_due);
+            dynamicPanel.Controls.Add(details_sub);
             dynamicPanel.Controls.Add(pictureBox);
             //dynamicPanel.Controls.Add(checkBox1);
             Controls.Add(dynamicPanel);
-            flowLayoutPanel1.Controls.Add(dynamicPanel);
+            panel3.Controls.Add(dynamicPanel);
         }
 
         private void Button_Click(object sender, EventArgs e)
@@ -177,6 +236,23 @@ namespace Task_System
                 //MessageBox.Show("ITS REVISION");
             }
             
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox2.Size = new Size(100, textBox1.Lines.Count()*17);
+            String[] f = textBox1.Text.Split(',');
+            String s = string.Empty;
+            for (int i = 0; i < f.Count(); i++)
+            {
+                s += f[i]+ Environment.NewLine;
+            }
+            textBox2.Text = s;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Lines.Count().ToString());
         }
     }
 }
